aa02af37238ee07d6ed9d97909c40ee572ad1c71
MouadSadik <sadikmoad90@gmail.com>
2025-03-23 00:20:38 +0100

    fix client

--- Clients.c
+++ Clients.c
@@ -1,22 +1,7 @@
 #include<stdio.h>
 #include<Clients.h>
 
-<<<<<<< HEAD
-struct client {
-    int id_Client;
-    char *Nom;
-    char *Prenom;
-    char *Email;
-    char *Adresse;
-    char *Nationalite;
-    int Code_Postale;
-    char *Ville_Client;
-};
-typedef struct client Client;
 
-
-=======
-static unsigned int NBC = 0 ; // Variable globale : Compteur de clients devant �tre auto-incr�ment� 
-static unsigned int CC = 0 ; // Cle primaire de la structure Client
-static Client * TC = NULL ; // Variable globale : Pointeur pour creer un tableau dynamique de clients
->>>>>>> 59d104493f378a843b2f08405b8a58cffa207989
+static unsigned int NB_Client = 0 ; 
+static unsigned int CClient= 0 ; 
+static Client * TClient = NULL ;
