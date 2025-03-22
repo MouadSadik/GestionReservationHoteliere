9195155f9f3fdeda3edd0c9e651adcf58157e1dd
MouadSadik <sadikmoad90@gmail.com>
2025-03-22 23:24:05 +0100

    first commit

---
+++ Clients.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+struct client {
+    int id_Client;
+    char *Nom;
+    char *Prenom;
+    char *Email;
+    char *Adresse;
+    char *Nationalite;
+    int Code_Postale;
+    char *Ville_Client;
+};
+typedef struct client Client;
